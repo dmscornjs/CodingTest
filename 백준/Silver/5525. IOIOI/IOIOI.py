bc7e0fba19bf902065d868ae140b7fc03201831a
dmscornjs <49949138+dmscornjs@users.noreply.github.com>
2023-01-27 10:26:03 +0900

    [Silver I] Title: IOIOI, Time: 112 ms, Memory: 114328 KB -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -4,7 +4,8 @@ s = input()
 
 p = 'IO' * n + 'I'
 ans = 0
-for i in range(m-len(p)):
-    if s[i:i+len(p)] == p:
-        ans +=1
+for i in range(m-(2*n+1)):
+    if s[i] == 'I':
+        if s[i:i+(2*n+1)] == p:
+            ans +=1
 print(ans)
