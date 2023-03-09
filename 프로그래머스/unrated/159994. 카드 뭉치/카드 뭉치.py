47adb45455295c50945e4254949e99baa961d50e
dmscornjs <49949138+dmscornjs@users.noreply.github.com>
2023-03-09 13:40:17 +0900

    [unrated] Title: 카드 뭉치, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.py
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.py
@@ -1,7 +1,7 @@
 def solution(cards1, cards2, goal):
     answer = 'Yes'
     for i in goal:
-        if len(cards1)>0 and i == cards1[0]:
+        if len(cards1)>0 and i == cards1[0] :
             cards1.pop(0)
         elif len(cards2)>0 and i == cards2[0]:
             cards2.pop(0)
