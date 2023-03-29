d62b65ea9ba7905ccec21986bcf3a9bed9e69fdf
dmscornjs <49949138+dmscornjs@users.noreply.github.com>
2023-03-29 11:59:21 +0900

    [Silver I] Title: 미로 탐색, Time: 80 ms, Memory: 34176 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -0,0 +1,29 @@
+from collections import deque
+import sys
+#input = sys.stdin.readline
+n, m = map(int, input().split())
+graph = []
+for _ in range(n):
+    graph.append(list(map(int, input())))
+
+dx = [1,0,-1,0]
+dy = [0,-1,0,1]
+
+def bfs(x, y):
+    queue = deque()
+    queue.append((x, y))
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if nx < 0 or ny < 0 or nx >= n or ny >= m:
+                continue
+            if graph[nx][ny] == 0:
+                continue
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append((nx, ny))
+    return graph[n-1][m-1]
+
+print(bfs(0, 0))
