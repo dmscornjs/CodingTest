e087bd72319d9086afb842f88ba7216743032fba
dmscornjs <49949138+dmscornjs@users.noreply.github.com>
2023-05-02 23:29:11 +0900

    [Gold V] Title: 톱니바퀴, Time: 148 ms, Memory: 114436 KB -BaekjoonHub

--- 백준/Gold/14891. 톱니바퀴/톱니바퀴.py
+++ 백준/Gold/14891. 톱니바퀴/톱니바퀴.py
@@ -1,58 +1,60 @@
+import sys
 from collections import deque
+input = sys.stdin.readline
+t1 = deque([c for c in input().rstrip()])
+t2 = deque([c for c in input().rstrip()])
+t3 = deque([c for c in input().rstrip()])
+t4 = deque([c for c in input().rstrip()])
 
-t1 = deque([c for c in input()])
-t2 = deque([c for c in input()])
-t3 = deque([c for c in input()])
-t4 = deque([c for c in input()])
 k = int(input())
-for i in range(k):
-    tn, direct = map(int,input().split())
-    if tn == 1:
-        a = t1[2]
-        t1.rotate(direct)
-        if a != t2[6]:
-            a = t2[2]
-            t2.rotate(-direct)
-            if a != t3[6]:
-                a = t3[2]
-                t3.rotate(direct)
-                if a != t4[6]:
-                    t4.rotate(-direct)
-    
-    elif tn == 2:
-        a = t2[6]
-        b = t2[2]
-        t2.rotate(direct)
-        if a != t1[2]:
-            t1.rotate(-direct)
-        if b != t3[6]:
-            b = t3[2]
-            t3.rotate(-direct)
-            if b != t4[6]:
-                t4.rotate(direct)
+for _ in range(k):
+    tn, d = map(int, input().split())
 
+    if tn == 1:
+        if t1[2] != t2[6]:
+            t1.rotate(d)
+            if t2[2] != t3[6]:
+                t2.rotate(-d)
+                if t3[2] != t4[6]:
+                    t3.rotate(d)
+                    t4.rotate(-d)
+                else:
+                    t3.rotate(d)
+            else:
+                t2.rotate(-d)
+        else:
+            t1.rotate(d)
+    elif tn == 2:
+        if t2[2] != t3[6]:
+            if t3[2] != t4[6]:
+                t4.rotate(d)
+            t3.rotate(-d)
+        if t2[6] != t1[2]:
+            t1.rotate(-d) 
+        t2.rotate(d)
+    
     elif tn == 3:
-        a = t3[6]
-        b = t3[2]
-        t3.rotate(direct)
-        if b != t4[6]:
-            t4.rotate(-direct)
-        if a != t2[2]:
-            a = t2[6]
-            t2.rotate(-direct)
-            if a != t1[2]:
-                t1.rotate(direct)
+        if t3[6] != t2[2]:
+            if t2[6] != t1[2]:
+                t1.rotate(d)
+            t2.rotate(-d)
+        if t3[2] != t4[6]:
+            t4.rotate(-d) 
+        t3.rotate(d)
 
     elif tn == 4:
-        a = t4[6]
-        t4.rotate(direct)
-        if a != t3[2]:
-            a = t3[6]
-            t3.rotate(-direct)
-            if a != t2[2]:
-                a = t2[6]
-                t2.rotate(direct)
-                if a != t1[2]:
-                    t1.rotate(-direct)
+        if t4[6] != t3[2]:
+            t4.rotate(d)
+            if t3[6] != t2[2]:
+                t3.rotate(-d)
+                if t2[6] != t1[2]:
+                    t2.rotate(d)
+                    t1.rotate(-d)
+                else:
+                    t2.rotate(d)
+            else:
+                t3.rotate(-d)
+        else:
+            t4.rotate(d)
 
-print(int(t1[0]) + int(t2[0])*2 + int(t3[0])*4 + int(t4[0])*8)
+print(int(t1[0]) + (int(t2[0])*2) + (int(t3[0]) *4)+ (int(t4[0])*8))
